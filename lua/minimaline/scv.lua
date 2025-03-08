1e3a90743c1bf1aebd0dcab0f3bd808c665f0074
Stepan Kolovorotnyy <skolovorotnyy@10.211.33.182-vpn.dhcp.yndx.net>
2025-03-08 18:23:05 +0300

    fix

--- lua/minimaline/scv.lua
+++ lua/minimaline/scv.lua
@@ -5,7 +5,7 @@ local arc_source = require('minimaline.arc')
 
 function M.get_sc_branch()
     git_branch = git_source.get_git_branch()
-    arc_source = arc_source.get_arc_branch()
+    arc_branch = arc_source.get_arc_branch()
 
     if git_branch ~= "" then
         return git_branch
